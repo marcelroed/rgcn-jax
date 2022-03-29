5a83690c0d9f0a9123dd7b4426e97e07f0cdda22
Marcel Rød <marcelroed@gmail.com>
2022-03-29 11:19:52 +0100

    Push for Hashan

--- rgcn/experiments/link_prediction.py
+++ rgcn/experiments/link_prediction.py
@@ -60,18 +60,49 @@ def encode(edge_index, num_nodes):
     return row * num_nodes + col
 
 
+def encode_with_type(edge_index, num_nodes, edge_type):
+    row, col = edge_index[0], edge_index[1]
+    return edge_type * num_nodes * num_nodes + row * num_nodes + col
+
+
 def negative_sample(edge_index, num_nodes, num_negatives, key):
+    """Generates negative samples completely randomly (no corruption)"""
     # Generate random edges
-    rand = jrandom.randint(key, (2, num_negatives), 0, num_nodes)
+    rand = jrandom.randint(key, (2, num_negatives), 0, num_nodes)  # [num_negatives, ]
 
     real_coding = encode(edge_index, num_nodes)  # [num_edges, ]
-    rand_coding = encode(rand, num_nodes)  # [num_edges, ]
+    rand_coding = encode(rand, num_nodes)  # [num_negatives, ]
 
-    positive_samples_mask = jnp.isin(rand_coding, real_coding)
+    positive_samples_mask = jnp.isin(rand_coding, real_coding).reshape(1, num_negatives)  # [1, num_negatives]
 
     negative_samples = jnp.where(positive_samples_mask, jrandom.randint(key, (2, num_negatives), 0, num_nodes), rand)
 
-    return negative_samples
+    return negative_samples  # [2, num_negatives]
+
+
+def batched_negative_sample(edge_index, edge_type, num_nodes, key):
+    # Always generate one negative sample per positive sample
+    num_edges = edge_index.shape[1]
+    rand = jrandom.randint(key, (1, num_edges), 0, num_nodes)  # [1, num_edges]
+    rand2 = jrandom.randint(key, (1, num_edges), 0, num_nodes)  # [1, num_edges]
+
+    head_or_tail = jrandom.bernoulli(key, 0.5, (1, num_edges))  # [1, num_edges]
+    head_tail_mask = jnp.concatenate([head_or_tail, ~head_or_tail], axis=0)  # [2, num_edges]
+
+    maybe_negative_samples = jnp.where(head_tail_mask, rand, edge_index)  # [2, num_edges]
+
+    real_triples = jnp.concatenate([edge_index, edge_type.reshape(1, -1)], axis=0)  # [3, num_edges]
+    encoded_real_triples = encode_with_type(real_triples, num_nodes, edge_type)  # [num_edges, ]
+
+    maybe_negative_triples = jnp.concatenate([maybe_negative_samples, edge_type.reshape(1, -1)], axis=0)  # [3, num_edges]
+    maybe_negative_encoded_triples = encode_with_type(maybe_negative_triples, num_nodes, edge_type)  # [num_edges, ]
+
+    positive_samples_mask = jnp.isin(maybe_negative_encoded_triples, encoded_real_triples).reshape(1, num_edges)  # [1, num_edges]
+    head_tail_adjusted_positive_samples_mask = head_tail_mask * positive_samples_mask  # [2, num_edges]
+    definitely_negative_samples = jnp.where(head_tail_adjusted_positive_samples_mask, rand2, maybe_negative_samples)  # [2, num_edges]
+
+    return definitely_negative_samples
+
 
 
 def get_train_epoch_data(dataset: LinkPredictionWrapper, key):
@@ -93,6 +124,18 @@ def get_train_epoch_data(dataset: LinkPredictionWrapper, key):
 
     full_edge_index = jnp.concatenate((pos_edge_index, neg_edge_index), axis=-1)
     full_edge_type = jnp.concatenate((pos_edge_type, neg_edge_type), axis=-1)
+    pos_mask = jnp.concatenate((jnp.ones_like(pos_edge_type), jnp.zeros_like(neg_edge_type)))
+    return full_edge_index, full_edge_type, pos_mask
+
+
+@jax.jit
+def get_train_epoch_data_fast(pos_edge_index, pos_edge_type, num_nodes, key):
+    neg_edge_index = batched_negative_sample(edge_index=pos_edge_index, edge_type=pos_edge_type, num_nodes=num_nodes, key=key)
+    neg_edge_type = pos_edge_type
+
+    full_edge_index = jnp.concatenate((pos_edge_index, neg_edge_index), axis=-1)
+    full_edge_type = jnp.concatenate((pos_edge_type, neg_edge_type), axis=-1)
+
     pos_mask = jnp.concatenate((jnp.ones_like(pos_edge_type), jnp.zeros_like(neg_edge_type)))
     return full_edge_index, full_edge_type, pos_mask
 
@@ -157,9 +200,11 @@ def train():
     num_epochs = 50
 
     t = trange(num_epochs)
+    pos_edge_index, pos_edge_type = dataset.edge_index[:, dataset.train_idx], dataset.edge_type[dataset.train_idx]
+    num_nodes = dataset.num_nodes
     try:
         for i in t:
-            edge_index, edge_type, pos_mask = get_train_epoch_data(dataset, key)
+            edge_index, edge_type, pos_mask = get_train_epoch_data_fast(pos_edge_index, pos_edge_type, num_nodes, key)
             loss, grads = loss_fn(model, edge_index, edge_type, pos_mask)
             updates, opt_state = optimizer.update(grads, opt_state)
             mean_test_score = model(test_edge_index, test_edge_type).mean()
